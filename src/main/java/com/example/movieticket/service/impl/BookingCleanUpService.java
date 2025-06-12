7c69f22bf98e841454c53fb99685ea9d43aa0beb
Phong Pahmm <phongphp03@gmail.com>
2025-06-12 22:40:56 +0700

    fix bugs

--- src/main/java/com/example/movieticket/service/impl/BookingCleanUpService.java
+++ src/main/java/com/example/movieticket/service/impl/BookingCleanUpService.java
@@ -2,8 +2,11 @@ package com.example.movieticket.service.impl;
 
 import com.example.movieticket.common.BookingStatus;
 import com.example.movieticket.common.PaymentStatus;
+import com.example.movieticket.common.SeatStatus;
 import com.example.movieticket.model.Booking;
+import com.example.movieticket.model.BookingSeat;
 import com.example.movieticket.repository.BookingRepository;
+import com.example.movieticket.repository.BookingSeatRepository;
 import com.example.movieticket.repository.PaymentRepository;
 import jakarta.transaction.Transactional;
 import lombok.AccessLevel;
@@ -20,7 +23,7 @@ import java.util.List;
 public class BookingCleanUpService {
     BookingRepository bookingRepository;
     PaymentRepository paymentRepository;
-
+    BookingSeatRepository bookingSeatRepository;
     @Transactional
     public void cancelExpiredBookings() {
         LocalDateTime now = LocalDateTime.now();
@@ -28,6 +31,13 @@ public class BookingCleanUpService {
 
         for (Booking booking : expiredBookings) {
             booking.setStatus(BookingStatus.CANCELLED);
+
+            List<BookingSeat> bookingSeats = bookingSeatRepository.findByBooking(booking);
+            for (BookingSeat seat : bookingSeats) {
+                seat.setStatus(SeatStatus.AVAILABLE);
+            }
+            bookingSeatRepository.saveAll(bookingSeats);
+
             if (booking.getPayment() != null) {
                 booking.getPayment().setStatus(PaymentStatus.FAILED);
                 paymentRepository.save(booking.getPayment());
