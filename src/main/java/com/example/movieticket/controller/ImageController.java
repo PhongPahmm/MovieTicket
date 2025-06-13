cf225df53d0d0b67260c57645afc80909ff70776
Phong Pahmm <phongphp03@gmail.com>
2025-06-13 21:29:56 +0700

    update image

---
+++ src/main/java/com/example/movieticket/controller/ImageController.java
@@ -0,0 +1,44 @@
+package com.example.movieticket.controller;
+
+
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+import org.springframework.http.*;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+@RestController
+@RequestMapping("/images")
+public class ImageController {
+
+    private final Path uploadPath = Paths.get("uploads").toAbsolutePath().normalize();
+
+    @GetMapping("/{filename:.+}")
+    public ResponseEntity<Resource> getImage(@PathVariable String filename) {
+        try {
+            Path filePath = uploadPath.resolve(filename).normalize();
+            Resource resource = new UrlResource(filePath.toUri());
+            if (resource.exists()) {
+                String contentType = Files.probeContentType(filePath);
+                if (contentType == null) {
+                    contentType = "application/octet-stream";
+                }
+                return ResponseEntity.ok()
+                        .contentType(MediaType.parseMediaType(contentType))
+                        .body(resource);
+            } else {
+                return ResponseEntity.notFound().build();
+            }
+        } catch (MalformedURLException e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
+
